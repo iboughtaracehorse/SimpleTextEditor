6ec2a561ed3731f32e2844ff1370d5c80ceffbb0
Dariia Dushynska <ddushynska@kse.org.ua>
2024-05-17 09:51:30 +0300

    arrays added

--- Main.cpp
+++ Main.cpp
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main() {
-    char* input = 0;
-    size_t bufferSize = 256; // Initial buffer size
 
-    // Allocate memory for the input buffer
-    input = (char*)malloc(bufferSize * sizeof(char));
-    if (input == NULL) {
-        printf("Memory allocation failed. Exiting...\n");
-        return 1;
-    }
+	char userinput[20];
+	printf("hello. enter you string?\n");
+	fgets(userinput, 20, stdin);
+	printf("your string: %s\n", userinput);
 
-    // Prompt the user to enter a string
-    printf("Enter a string: ");
-    // Read the string from the console
-    fgets(input, bufferSize, stdin);
+	const size_t initialSize = 256;
+	char localText[initialSize][initialSize];
 
-    // Print the string
-    printf("You entered: %s", input);
+	char** text = (char**)malloc(initialSize * sizeof(char*));
+	for (int i = 0; i < 4; i++)
+		text[i] = (char*)malloc(initialSize * sizeof(char));
 
-    // Free dynamically allocated memory
-    free(input);
+	strcpy_s(text[0], initialSize, "Hello");
+	strcpy_s(text[1], initialSize, "World");
 
-    return 0;
+	text[1][0] = '!';
 
+	printf("%s, %s\n", text[0], text[1]);
+
+    // Free the allocated memory
+    for (int i = 0; i < 2; i++)
+        free(text[i]);
+    free(text);
+
+	return 0;
 }
