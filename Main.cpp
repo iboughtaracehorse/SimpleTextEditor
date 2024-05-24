525bb48c4d19a84feb8a8e2836bd68dff61db173
Dariia Dushynska <ddushynska@kse.org.ua>
2024-05-24 16:06:01 +0300

    multiple commands added(template)

--- Main.cpp
+++ Main.cpp
@@ -5,22 +5,29 @@
 enum Commands
 {
 	COMMAND_APPEND = 1,
-	COMMAND_INSERT = 2,
-	COMMAND_HELP
+	COMMAND_INSERT,
+	COMMAND_NEW,
+	COMMAND_SAVE,
+	COMMAND_LOAD,
+	COMMAND_SEARCH,
+	COMMAND_HELP,
+	COMMAND_EXIT,
+	COMMAND_UNKNOWN
 };
 
 void appendText(char** text, size_t initialSize, const char* newText);
 void help();
+int getCommand(char* userInput);
 
 int main() {
 
-	char userInput[7];
+	char userInput[20];
 	const size_t initialSize = 100;
 	char** text = (char**)malloc(initialSize * sizeof(char*));
 	// second dimension 
 	for (size_t i = 0; i < initialSize; i++) {
 		text[i] = (char*)malloc(initialSize * sizeof(char));
-		text[i][0] = '\0';  
+		text[i][0] = '\0';
 	}
 
 	strcpy_s(text[0], initialSize, "Hello");
@@ -28,15 +35,51 @@ int main() {
 
 	while (true)
 	{
-		printf("Enter a command or 'help' to see the list of available comands: ");
+		printf("Enter a command or 'help' to see the list of available commands: ");
 		scanf_s("%s", userInput);
+
+		int command = getCommand(userInput);
+
+		switch (command)
+		{
+			case COMMAND_APPEND: {
+				printf("Command is not available");
+				break;
+			}case COMMAND_INSERT: {
+				printf("Command is not available");
+				break;
+			}case COMMAND_NEW: {
+				printf("Command is not available");
+				break;
+			}case COMMAND_SAVE: {
+				printf("Command is not available");
+				break;
+			}case COMMAND_LOAD: {
+				printf("Command is not available");
+				break;
+			}case COMMAND_SEARCH: {
+				printf("Command is not available");
+				break;
+			}case COMMAND_HELP: {
+				printf("Command is not available");
+				break;
+			}case COMMAND_EXIT: {
+				printf("Command is not available");
+				break;
+			}case COMMAND_UNKNOWN: {
+				printf("AAAAAAAAAAAAAAAAAAAA");
+				break;
+			}
+			default:
+				printf("Unknown command. Please try again: ");
+				break;
+			}
 	}
 
-
-    // Free the allocated memory
-    for (int i = 0; i < initialSize; i++)
-        free(text[i]);
-    free(text);
+	// Free the allocated memory
+	for (int i = 0; i < initialSize; i++)
+		free(text[i]);
+	free(text);
 
 	return 0;
 }
@@ -54,6 +97,39 @@ void help() {
 
 	printf("  help    -- Display all available commands\n");
 	printf("  exit    -- Exit editor\n");
+}
+
+int getCommand(char* userInput)
+{
+	if (strcmp(userInput, "append") == 0) {
+		return COMMAND_APPEND;
+	}
+	else if (strcmp(userInput, "insert") == 0) {
+		return COMMAND_INSERT;
+	}
+	else if (strcmp(userInput, "new") == 0) {
+		return COMMAND_NEW;
+	}
+	else if (strcmp(userInput, "save") == 0) {
+		return COMMAND_SAVE;
+	}
+	else if (strcmp(userInput, "load") == 0) {
+		return COMMAND_LOAD;
+	}
+	else if (strcmp(userInput, "search") == 0) {
+		return COMMAND_SEARCH;
+	}
+	else if (strcmp(userInput, "help") == 0) {
+		return COMMAND_HELP;
+	}
+	else if (strcmp(userInput, "exit") == 0) {
+		return COMMAND_EXIT;
+	}
+
+
+	 else {
+		return COMMAND_UNKNOWN;
+		}
 }
 
 //void appendText(char* text, size_t initialSize, char* newText) {
